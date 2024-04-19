cf9997dd467f871f374666b5e28f56118c976a27
Luan-art <luanlf2003@gmail.com>
2024-04-19 15:52:29 -0300

    Add project files.

---
+++ tipoTriangulos/Program.cs
@@ -0,0 +1,39 @@
+﻿float ladoA = 0, ladoB = 0, ladoC = 0;
+
+do{
+    Console.WriteLine("Digite o lado de um triangulo: ");
+    ladoA = float.Parse(Console.ReadLine());
+} while (ladoA <= 0);
+
+do
+{
+    Console.WriteLine("Digite o outro lado de um triangulo: ");
+    ladoB = float.Parse(Console.ReadLine());
+} while (ladoB <= 0);
+
+
+do
+{
+     Console.WriteLine("Digite o último lado de um triangulo: ");
+     ladoC = float.Parse(Console.ReadLine());
+
+} while (ladoC <= 0);
+
+if (ladoA == ladoB && ladoB == ladoC)
+{
+    Console.WriteLine("triângulo equilátero ");
+
+}
+else
+{
+    if( (ladoA == ladoB && ladoB != ladoC) || (ladoA == ladoC && ladoC != ladoB) || (ladoC == ladoB && ladoB != ladoA))
+    {
+        Console.WriteLine("triângulo isósceles ");
+
+    }
+    else
+    {
+        Console.WriteLine("triângulo escaleno ");
+
+    }
+}
